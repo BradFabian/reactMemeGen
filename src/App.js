c6a87dd0cc1c9e4dc075a0aab603765260670f54
Brad Fabian <bradleofabian@gmail.com>
2020-10-20 13:37:49 -0400

    set up meme generator

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from "./components/Header";
+import MemeGenerator from "./components/MemeGenerator";
+
+
+export default function App() {
+  return (
+    <React.Fragment>
+       <Header />
+    <MemeGenerator />
+  
+    </React.Fragment>)
+   
+}
+
